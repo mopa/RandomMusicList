b4e0ee76f7e7e6c3931cb0aea5df383fcb4c2803
Pablo M <951188+mopa@users.noreply.github.com>
2019-05-21 13:01:41 +0200

    Testing

--- random_list.sh
+++ random_list.sh
@@ -9,9 +9,21 @@
 ###########################################################
 
 
-x=12345
-for((i=5;i>0;i--));do
-  ((r=RANDOM%i+1))
-  echo ${x:r-1:1}
-  x=${x:0:r-1}${x:r}
+#x=12345
+#for((i=5;i>0;i--));do
+#  ((r=RANDOM%i+1))
+#  echo ${x:r-1:1}
+#  x=${x:0:r-1}${x:r}
+#done
+
+
+range=10
+
+
+for ((i=0; i<=$range; i++));
+do
+  n=$((1 + RANDOM % $range))
+
+  prev[n]=1
+  echo $prev
 done
